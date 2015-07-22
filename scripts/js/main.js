83441e466e6d009ce03c64a20ca6e78a7f06159e
Benjamin <benjamin@trax-air.com>
2015-07-22 11:55:16 +0200

    Update main

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,169 +1,219 @@
 'use strict';
 /* global WaveSurfer, YT */
 
-WaveSurfer.TraxitV2 = {
-  init: function(pWavesurfer) {
-        this.wavesurfer = pWavesurfer;
-        console.log('init');
+var TraxitSurferV2 = {
+  init: function(pParams) {
+    //console.log('init()');
+    this.wavesurfer = pParams.wavesurfer;
   },
 
   /**
   * Load a media
   *   Params :
-  * pSourceType {String} : where the media comes from, ex: 'soundcloud', 'youtube'
+  *   pSourceType {String} : where the media comes from, ex: 'soundcloud', 'youtube'
   *   pSource {String} : the media-to-load's url(for Youtube, provide video ID)
   */
   load: function(pSourceType, pSource, pWaveform) {
-    console.log('traxit load()')
+    //console.log('load()');
     var me = this;
-    var mediaElementReadyEvent = new Event('mediaElementReady');
-    this.mediaElement = {
-        play: function() {},
-        pause: function() {}
-    }
+    var mediaElementReadyEvent = document.createEvent('CustomEvent');
+    mediaElementReadyEvent.initCustomEvent('mediaElementReady', false, false, null);
+    this.currentTime = 0;
     //Provide precalculated waveform to wavesurfer
     this.wavesurfer.backend.peaks = pWaveform;
     this.wavesurfer.drawBuffer();
 
     var mediaElement;
     if(pSourceType === 'soundcloud') {
-      mediaElement = document.createElement('audio');
-      mediaElement.src = pSource;
+        mediaElement = document.createElement('audio');
+        mediaElement.src = pSource;
 
-      console.log('mediaElement created');
-      mediaElement.addEventListener('timeupdate', function() {
-        me.onProgress();
-      });
+        mediaElement.addEventListener('timeupdate', function() {
+          me.onProgress();
+        });
 
-      mediaElement.seekTo = function(pTime) {
-        mediaElement.currentTime = pTime;
-      };
+        mediaElement.seekTo = function(pTime) {
+          mediaElement.currentTime = pTime;
+        };
 
-      mediaElement.addEventListener('canplay', function() {
-        me.onMediaElementReady();
-      });
+        mediaElement.addEventListener('canplay', function() {
+          me.onMediaElementReady();
+        });
 
-      this.mediaElement = mediaElement;
-      document.dispatchEvent(mediaElementReadyEvent);
+        this.play = function() {
+          mediaElement.play();
+        };
+
+        this.pause = function() {
+          mediaElement.pause();
+        };
+
+        this.mediaElement = mediaElement;
+        document.dispatchEvent(mediaElementReadyEvent);
     } else if (pSourceType === 'youtube') { 
       //Loading YT player
+      
       this.loadYoutubePlayer(pSource);
 
       //Fired when youtube player is ready
         document.addEventListener('youtubeReady', function(pEvent) {
-          //On fait ça propre, on enlève l'écouteur :)
-          document.removeEventListener('youtubeReady');
+            //console.log('youtubeReady');
+            //On fait ça propre, on enlève l'écouteur :)
+            document.removeEventListener('youtubeReady');
 
-          //pEvent.detail contains the instance of the youtube Player
-          mediaElement = pEvent.detail;
+            //pEvent.detail contains the instance of the youtube Player
+            mediaElement = pEvent.detail;
 
-          var progressInterval; 
-          //Reference to the setInterval which update the waveform
-          //Recreation of a 'timeupdate' event
-          mediaElement.addEventListener('onStateChange',function(pEvent) {
-            console.log(pEvent.data);
-            if(pEvent.data === 1) {
-              progressInterval = setInterval(function() {
-                me.onProgress();
-              }, 100);
-            } else {
-              clearInterval(progressInterval);
-            }
-          });
-          
-          //Creation de la couche d'abstraction youtube pour avoir les mêmes méthodes partout
-          mediaElement.play = function() {
-            mediaElement.playVideo();
-          };
+            var progressInterval; 
+            //Reference to the setInterval which update the waveform
+            //Recreation of a 'timeupdate' event
+            mediaElement.addEventListener('onStateChange',function(pEvent) {
+              //console.log(pEvent.data);
+              if(pEvent.data === 1) {
+                progressInterval = setInterval(function() {
+                  me.onProgress();
+                }, 200);
+              } else {
+                clearInterval(progressInterval);
+              }
+            });
+            
+            //Creation de la couche d'abstraction youtube pour avoir les mêmes méthodes partout
+            me.play = function() {
+                //console.log('youtubePlay');
+              mediaElement.playVideo();
+            };
 
-          mediaElement.pause = function() {
-            mediaElement.pauseVideo();
-          };
+            me.pause = function() {
+              mediaElement.pauseVideo();
+            };
 
-          mediaElement.duration = mediaElement.getDuration();
+            Object.defineProperty(mediaElement, 'duration', { get: function () { return this.getDuration(); } });
 
-          //Création de la prop currentTime qui renvoie le résultat de la fonction getCurrentTime du player youtube
-          Object.defineProperty(mediaElement, 'currentTime', { get: function () { return this.getCurrentTime(); } });
+            //Création de la prop currentTime qui renvoie le résultat de la fonction getCurrentTime du player youtube
+            Object.defineProperty(mediaElement, 'currentTime', { get: function () { return this.getCurrentTime(); } });
 
-          me.mediaElement = mediaElement;
-          document.dispatchEvent(mediaElementReadyEvent);
+
+            me.mediaElement = mediaElement;
+            document.dispatchEvent(mediaElementReadyEvent);
         });
     }
 
     document.addEventListener('mediaElementReady', function() {
       me.onMediaElementReady();
     });
-    
   },
-  onMediaElementReady: function(pCallback) {
+  play: function() {},
+  pause: function() {},
+  onMediaElementReady: function() {
+    document.removeEventListener('mediaElementReady');
     var me = this;
+    this.duration = this.mediaElement.duration;
     this.wavesurfer.drawer.on('click', function(pEvent, pProgress) {
       me.seekTo(pProgress);
     });
-    if(pCallback && typeof pCallback === 'function') pCallback();
+    if(this.onMediaElementReadyCallback) this.onMediaElementReadyCallback();
   },
   onProgress: function() {
     var lProgress = this.mediaElement.currentTime / this.mediaElement.duration;
     this.wavesurfer.drawer.progress(lProgress);
+    this.currentTime = this.mediaElement.currentTime;
     this.wavesurfer.fireEvent('progress');
   },
   seekTo: function(pProgress) {
     this.wavesurfer.drawer.progress(pProgress);
     this.mediaElement.seekTo(pProgress * this.mediaElement.duration);
-    this.mediaElement.play();
+    this.play();
+    this.setPlayingMark(null);
+    if(this.seekToCallback) this.seekToCallback();
+  },
+  getPercentages: function(pTime) {
+    return pTime / this.duration;
   },
   addMark: function() {
 
   },
   addAllMarks: function(pTracklist) {
+    if(!pTracklist) return;
     var me = this;
+    if(!this.marks) this.marks = [];
+    this.removeAllMarks();
+
+    var waveformContainer = document.querySelector('#waveform');
     for(var i = 0; i < pTracklist.length; i++) {
-      var lMark = document.createElement('div');
-      var waveformContainer = document.querySelector('#waveform');
-      waveformContainer.appendChild(lMark);
-      lMark.id = 'mark_'+i;
-      lMark.style.width = '1px';
-      lMark.style.height = '100px';
-      lMark.style.position = 'absolute';
-      lMark.style.top = '0px';
+        var lMark;
+        if(this.marks[i]) {
+          lMark = this.marks[i];
+        } else {
+          lMark = document.createElement('div');
+          this.addClass(lMark, 'mark');
+          this.marks[i] = lMark;
+        }
+        
+       
+        waveformContainer.appendChild(lMark);
+        lMark.id = 'mark_'+i;
 
-      var leftOffset = pTracklist[i].start / this.mediaElement.duration * waveformContainer.offsetWidth;
-      lMark.style.left = leftOffset + 'px';
-      lMark.style.display = 'inline-block';
-      lMark.style.zIndex = '5';
-      lMark.style.backgroundColor = 'rgb(200, 200, 200)';
+        var leftOffset = pTracklist[i].start / this.duration * waveformContainer.offsetWidth;
+        lMark.style.left = leftOffset + 'px';
 
-      var lIcon = document.createElement('div');
-      lMark.appendChild(lIcon);
-      lIcon.style.width = '40px';
-      lIcon.style.height = '40px';
-      lIcon.style.position = 'relative';
-      lIcon.style.top = '100px';
-      lIcon.style.backgroundColor = 'rgb(100, 100, 100)';
-      if(pTracklist[i].track)
-        lIcon.style.backgroundImage = pTracklist[i].track.cover ? 'url("'+ pTracklist[i].track.cover +'")' : 'url("assets/img/notfound.png")';
+        var lIcon = document.createElement('div');
+        this.addClass(lIcon, 'iconMark');
 
-      lIcon.style.backgroundSize = 'contain';
-
-      lIcon.addEventListener('mouseenter', me.onMouseEnterMark);
-      lIcon.addEventListener('mouseout', me.onMouseOutMark);
+        lMark.appendChild(lIcon);
+        if(pTracklist[i].track) {
+            if(pTracklist[i].track.cover_url)
+                lIcon.style.backgroundImage = 'url("'+ pTracklist[i].track.cover_url +'")';
+            else 
+                this.addClass(lIcon, 'cover_not_found');
+        } else {
+            this.addClass(lIcon, 'not_found');
+        }
     }
   },
-  onMouseEnterMark: function(pEvent) {
-    pEvent.target.style.width = '80px';
-    pEvent.target.style.height = '80px';
-    pEvent.target.parentNode.style.zIndex = '10';
-    pEvent.target.style.transform = 'translate(0, -50%)';
+  addClass: function(pElement, pClass) {
+    if(pElement.className.indexOf(pClass) === -1)
+        pElement.className += ' '+pClass;
   },
-  onMouseOutMark: function(pEvent) {
-    pEvent.target.style.width = '40px';
-    pEvent.target.style.height = '40px';
-    pEvent.target.parentNode.style.zIndex = '5';
-    pEvent.target.style.transform = 'translate(0, 0)';
+  removeClass: function(pElement, pClass) {
+    var lRegex = new RegExp('(?:^|\s)' + pClass + '(?!\S)', 'g');
+    pElement.className = pElement.className.replace(pClass , '');
+  },
+  removeAllMarks: function() {
+    for(var i = this.marks.length - 1; i >= 0; i--) {
+      var mark = document.querySelector('#mark_'+i);
+      mark.remove();
+      this.marks.splice(i, 1);
+    }
+  },
+  setPlayingMark: function(pIdMark) {
+
+    if(pIdMark === null) {
+        var lPlayingMark = document.querySelector('#mark_' + this.idPlayingMark);
+        this.removeClass(lPlayingMark, ' playing');
+        var lPlayingIcon = lPlayingMark.querySelector('div');
+        this.removeClass(lPlayingIcon, ' playing');
+        return;
+    }
+    var lMark = document.querySelector('#mark_' + pIdMark);
+    var lIcon = lMark.querySelector('div');
+    this.addClass(lMark, 'playing');
+    this.addClass(lIcon, 'playing');
+
+    if(this.idPlayingMark !== undefined) {
+        var lPlayingMark = document.querySelector('#mark_' + this.idPlayingMark);
+        this.removeClass(lPlayingMark, ' playing');
+        var lPlayingIcon = lPlayingMark.querySelector('div');
+        this.removeClass(lPlayingIcon, 'playing');
+    }
+
+
+    this.idPlayingMark = pIdMark;
   },
   loadYoutubePlayer: function(pUrl) {
+    //console.log('load youtube script');
     var tag = document.createElement('script');
-    tag.src = "http://www.youtube.com/iframe_api";
+    tag.src = "https://www.youtube.com/iframe_api";
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     var player;
@@ -181,6 +231,7 @@ WaveSurfer.TraxitV2 = {
       }
 
       function onPlayerReady(event) {
+        //console.log('onPlayerReady()');
         var ytReadyEvent = new CustomEvent('youtubeReady', {'detail': player});
         document.dispatchEvent(ytReadyEvent);
       }
